d307a5a3662001d6a5308d1498c67d8735de70f5
ssong-eu <sangyoon.song@gmail.com>
2019-02-14 15:19:08 +0000

    problem with auth0 setup, fixing externally

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button } from 'semantic-ui-react'
+import { Menu } from 'semantic-ui-react'
 import Auth from './Auth.js'
 
 const auth = new Auth()
@@ -8,7 +8,13 @@ export default class Main extends React.Component {
   render () {
     return (
       <React.Fragment>
-        {auth.login()}
+        <Menu>
+          {
+            auth.isAuthenticated() === false // issue with redirection and fetching localStorage data.
+              ? <Menu.Item onClick={auth.login}>Log In</Menu.Item>
+              : <Menu.Item onClick={auth.logout}>Log Out</Menu.Item>
+          }
+        </Menu>
       </React.Fragment>
     )
   }
